75fe37ab010483e3abd05a93fbfe999657dd6e32
Gnutttggg <133179243+Gnutttggg@users.noreply.github.com>
2023-06-14 23:22:44 -0400

    Delete demo.js

--- demo.js
+++
@@ -1,6 +0,0 @@
-let audioDataUrl;
-const currentGridItem = audioPlayer.parentNode;
-const demoButton = document.createElement("button");
-demoButton.classList.add("demo-button");
-demoButton.innerHTML = "Demo";
-currentGridItem.appendChild(demoButton);
